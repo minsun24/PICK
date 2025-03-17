cd845094bb1152a550ad499b530162144df55997
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-03-18 00:49:16 +0900

    Style : 컨벤션에 맞게 쉼표 위치 수정

--- db/report/CreateReportTable.sql
+++ db/report/CreateReportTable.sql
@@ -7,33 +7,33 @@ DROP TABLE IF EXISTS REGULATION CASCADE;
 -- REPORT_REASON 테이블
 CREATE TABLE IF NOT EXISTS REPORT_REASON
 (
-    id          INT  AUTO_INCREMENT         COMMENT '신고 사유 번호',
-    reason      TEXT NOT NULL UNIQUE        COMMENT '신고 사유명',
-    CONSTRAINT pk_id PRIMARY KEY (id)
+    id          INT  AUTO_INCREMENT         COMMENT '신고 사유 번호'
+,   reason      TEXT NOT NULL UNIQUE        COMMENT '신고 사유명'
+,   CONSTRAINT pk_id PRIMARY KEY (id)
 );
 
 -- REPORT 테이블
 CREATE TABLE IF NOT EXISTS REPORT
 (
-    id                  INT AUTO_INCREMENT                      COMMENT '신고 번호',
-    date                VARCHAR(255) NOT NULL                   COMMENT '신고 신청일',
-    status              ENUM('처리중', '보류', '승인') NOT NULL    COMMENT '처리 상태',
-    category            ENUM('회원', '게시글', '댓글') NOT NULL    COMMENT '신고 유형',
-    reported_id         INTEGER NOT NULL                        COMMENT '신고 대상 id',
-    report_reason_id    INTEGER NOT NULL                        COMMENT '신고 사유 번호',
-    member_id           INTEGER NOT NULL                        COMMENT '신고 신청자',
-    CONSTRAINT pk_id PRIMARY KEY (id),
-    CONSTRAINT fk_report_report_reason_id FOREIGN KEY (report_reason_id) REFERENCES REPORT_REASON (id),
-    CONSTRAINT fk_report_member_id FOREIGN KEY (member_id) REFERENCES MEMBER (id)
+    id                  INT AUTO_INCREMENT                      COMMENT '신고 번호'
+,   date                VARCHAR(255) NOT NULL                   COMMENT '신고 신청일'
+,   status              ENUM('처리중', '보류', '승인') NOT NULL    COMMENT '처리 상태'
+,   category            ENUM('회원', '게시글', '댓글') NOT NULL    COMMENT '신고 유형'
+,   reported_id         INTEGER NOT NULL                        COMMENT '신고 대상 id'
+,   report_reason_id    INTEGER NOT NULL                        COMMENT '신고 사유 번호'
+,   member_id           INTEGER NOT NULL                        COMMENT '신고 신청자'
+,   CONSTRAINT pk_id PRIMARY KEY (id)
+,   CONSTRAINT fk_report_report_reason_id FOREIGN KEY (report_reason_id) REFERENCES REPORT_REASON (id)
+,   CONSTRAINT fk_report_member_id FOREIGN KEY (member_id) REFERENCES MEMBER (id)
 );
 
 -- REGULATION 테이블
 CREATE TABLE IF NOT EXISTS REGULATION
 (
-    id              INT AUTO_INCREMENT           COMMENT '규제 번호',
-    start_date      VARCHAR(255) NOT NULL        COMMENT '시작 날짜',
-    end_date        VARCHAR(255) NOT NULL        COMMENT '종료 날짜',
-    member_id       INTEGER NOT NULL             COMMENT '규제 대상 회원',
-    CONSTRAINT pk_id PRIMARY KEY (id),
-    CONSTRAINT fk_regulation_member_id FOREIGN KEY (member_id) REFERENCES MEMBER (id)
+    id              INT AUTO_INCREMENT           COMMENT '규제 번호'
+,   start_date      VARCHAR(255) NOT NULL        COMMENT '시작 날짜'
+,   end_date        VARCHAR(255) NOT NULL        COMMENT '종료 날짜'
+,   member_id       INTEGER NOT NULL             COMMENT '규제 대상 회원'
+,   CONSTRAINT pk_id PRIMARY KEY (id)
+,   CONSTRAINT fk_regulation_member_id FOREIGN KEY (member_id) REFERENCES MEMBER (id)
 );
