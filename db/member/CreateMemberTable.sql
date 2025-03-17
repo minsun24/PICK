2a7b0c6cebb0401058cce675de1bd53016c1c066
Gombo2 <rhtjddus0502@gmail.com>
2025-03-17 21:59:41 +0900

    REFACTOR: 멤버 도메인 테이블 생성 수정
    - 변경 내용 1 : sql 파일명 CreateMemberTable로 변경
    - 변경 내용 2 : fk 제약조건 생성
    - 변경 내용 3 : UNIQUE 조건 빠진 부분 채움
    - 변경 내용 4 : 프로필 이미지 테이블 삭제 및 프로필 테이블에 속성으로 이미지 경로 추가
    - 변경 내용 5 : 기존 프로필 유무 boolean 타입 속성 삭제

--- db/member/CreateMemberTable.sql
+++ db/member/CreateMemberTable.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS MEMBER_PROFILE_PAGE CASCADE;
 DROP TABLE IF EXISTS MEMBER_PROGRAMMING_LANGUAGE CASCADE;
 DROP TABLE IF EXISTS PROGRAMMING_LANGUAGE CASCADE;
 
-
 CREATE TABLE IF NOT EXISTS MEMBER
 (
     id             INTEGER      NOT NULL AUTO_INCREMENT
@@ -17,12 +16,12 @@ CREATE TABLE IF NOT EXISTS MEMBER
 ,   status         TINYINT      NOT NULL COMMENT '활성화, 비활성화(정지), 탈퇴 한번에 관리 (Enum)'
 ,   reported_count INTEGER      NOT NULL COMMENT '조회 덜하기 위해 회원 테이블 속성 추가'
 ,   user_grant     TINYINT      NOT NULL COMMENT 'ADMIN, MEMBER'
-,   CONSTRAINT pk_id PRIMARY KEY (id)
+,   CONSTRAINT pk_member_id PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS MEMBER_PROFILE_PAGE
 (
-    id               INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY
+    id               INTEGER      NOT NULL AUTO_INCREMENT
 ,   exp              INTEGER      NOT NULL
 ,   level            INTEGER      NOT NULL
 ,   introduce        TEXT         NULL
@@ -31,25 +30,24 @@ CREATE TABLE IF NOT EXISTS MEMBER_PROFILE_PAGE
 ,   score            INTEGER      NULL
 ,   image_path       VARCHAR(255) NULL UNIQUE
 ,   member_id        INTEGER      NOT NULL
-,   CONSTRAINT pk_id PRIMARY KEY (id)
+,   CONSTRAINT pk_member_profile_page_id PRIMARY KEY (id)
 ,   CONSTRAINT fk_member_profile_page_member_id FOREIGN KEY (member_id) REFERENCES MEMBER (id)
-);
-
-
-CREATE TABLE IF NOT EXISTS MEMBER_PROGRAMMING_LANGUAGE
-(
-    id                      INTEGER NOT NULL AUTO_INCREMENT
-,   programming_language_id INTEGER NOT NULL
-,   member_profile_page_id  INTEGER NOT NULL
-,   CONSTRAINT pk_id PRIMARY KEY (id)
-,   CONSTRAINT fk_member_programming_language_programming_language_id FOREIGN KEY (programming_language_id) REFERENCES programming_language (id)
-,   CONSTRAINT fk_member_programming_member_profile_page_id FOREIGN KEY (member_profile_page_id) REFERENCES member_programming_language (id)
 );
 
 CREATE TABLE IF NOT EXISTS PROGRAMMING_LANGUAGE
 (
-    id         INTEGER      NOT NULL AUTO_INCREMENT
+id         INTEGER      NOT NULL AUTO_INCREMENT
 ,   language   VARCHAR(255) NOT NULL UNIQUE
 ,   is_deleted VARCHAR(4)   NOT NULL DEFAULT 'N'
-,   CONSTRAINT pk_id PRIMARY KEY (id)
+,   CONSTRAINT pk_programming_language_id PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS MEMBER_PROGRAMMING_LANGUAGE
+(
+id                      INTEGER NOT NULL AUTO_INCREMENT
+,   programming_language_id INTEGER NOT NULL
+,   member_profile_page_id  INTEGER NOT NULL
+,   CONSTRAINT pk_member_programming_language_id PRIMARY KEY (id)
+,   CONSTRAINT fk_member_programming_language_programming_language_id FOREIGN KEY (programming_language_id) REFERENCES PROGRAMMING_LANGUAGE (id)
+,   CONSTRAINT fk_member_programming_member_profile_page_id FOREIGN KEY (member_profile_page_id) REFERENCES MEMBER_PROFILE_PAGE (id)
 );
