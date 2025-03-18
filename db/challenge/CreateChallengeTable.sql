0caf01365c75846acb27725000822217998e34b9
HMYIEN <bsm0407@naver.com>
2025-03-18 15:58:13 +0900

    Refactor: Challenge 도메인 관련 테이블 제약조건 수정, 컬럼명 삭제 및 수정
    일일미션 테이블 날짜 삭제
    회원별 일일미션 달성 날짜 컬럼명 수정 -> accepted_date

--- db/challenge/CreateChallengeTable.sql
+++ db/challenge/CreateChallengeTable.sql
@@ -7,75 +7,89 @@ DROP TABLE IF EXISTS ACHIEVEMENT;
 DROP TABLE IF EXISTS CHALLENGE;
 
 -- 챌린지 테이블
-CREATE TABLE IF NOT EXISTS CHALLENGE (
-    id   INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS CHALLENGE
+(
+    id   INTEGER      NOT NULL AUTO_INCREMENT
+,   name VARCHAR(255) NOT NULL
+,   CONSTRAINT pk_challenge_id PRIMARY KEY (id)
+,   CONSTRAINT uni_challenge_name UNIQUE (name)
 );
 
 -- 도전 과제 테이블
-CREATE TABLE IF NOT EXISTS ACHIEVEMENT (
-    id           INTEGER    NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    requirement  INTEGER    NOT NULL,
-    exp          INTEGER    NOT NULL,
-    is_deleted   VARCHAR(4) NOT NULL DEFAULT 'N',
-    challenge_id INTEGER    NOT NULL,
-    FOREIGN KEY (challenge_id) REFERENCES CHALLENGE(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS ACHIEVEMENT
+(
+    id           INTEGER    NOT NULL AUTO_INCREMENT
+,   requirement  INTEGER    NOT NULL
+,   exp          INTEGER    NOT NULL
+,   is_deleted   VARCHAR(4) NOT NULL DEFAULT 'N'
+,   challenge_id INTEGER    NOT NULL
+,   CONSTRAINT pk_achievement_id PRIMARY KEY (id)
+,   CONSTRAINT fk_achievement_challenge_id FOREIGN KEY (challenge_id) REFERENCES challenge(id)
 );
 
 -- 일일 미션 테이블
-CREATE TABLE IF NOT EXISTS DAILY_MISSION (
-    id           INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    content      TEXT         NOT NULL,
-    target_date  VARCHAR(255) NOT NULL,
-    exp_point    INTEGER      NOT NULL,
-    is_deleted   VARCHAR(4)   NOT NULL DEFAULT 'N',
-    challenge_id INTEGER      NOT NULL,
-    FOREIGN KEY (challenge_id) REFERENCES CHALLENGE(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS DAILY_MISSION
+(
+    id           INTEGER      NOT NULL AUTO_INCREMENT
+,   content      TEXT         NOT NULL
+,   exp_point    INTEGER      NOT NULL
+,   s_deleted   VARCHAR(4)   NOT NULL DEFAULT 'N'
+,   challenge_id INTEGER      NOT NULL
+,   CONSTRAINT pk_daily_mission_id PRIMARY KEY (id)
+,   CONSTRAINT fk_daily_mission_challenge_id FOREIGN KEY (challenge_id) REFERENCES challenge(id)
 );
 
 -- 뱃지 테이블
-CREATE TABLE IF NOT EXISTS BADGE (
-    id           INTEGER    NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    requirement  INTEGER,
-    description  TEXT       NOT NULL,
-    is_deleted   VARCHAR(4) NOT NULL DEFAULT 'N',
-    challenge_id INTEGER    NOT NULL,
-    FOREIGN KEY (challenge_id) REFERENCES CHALLENGE(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS BADGE
+(
+    id           INTEGER    NOT NULL AUTO_INCREMENT
+,   requirement  INTEGER
+,   description  TEXT       NOT NULL
+,   is_deleted   VARCHAR(4) NOT NULL DEFAULT 'N'
+,   challenge_id INTEGER    NOT NULL
+,   CONSTRAINT pk_badge_id PRIMARY KEY (id)
+,   CONSTRAINT fk_badge_challenge_id FOREIGN KEY (challenge_id) REFERENCES challenge(id)
 );
 
 -- 회원별 도전 과제 테이블
-CREATE TABLE IF NOT EXISTS MEMBER_ACHIEVEMENT (
-    id             INTEGER       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    achieved_date  VARCHAR(255),
-    progress       INTEGER,
-    achieved_time  INTEGER,
-    achievement_id INTEGER       NOT NULL,
-    member_id      INTEGER       NOT NULL,
-    FOREIGN KEY (achievement_id) REFERENCES ACHIEVEMENT(id) ON DELETE CASCADE,
-    FOREIGN KEY (member_id) REFERENCES MEMBER(id) ON DELETE CASCADE,
-    CONSTRAINT unique_member_achievement UNIQUE (member_id, achievement_id)
+CREATE TABLE IF NOT EXISTS MEMBER_ACHIEVEMENT
+(
+    id             INTEGER       NOT NULL AUTO_INCREMENT
+,   achieved_date  VARCHAR(255)
+,   progress       INTEGER
+,   achieved_time  INTEGER
+,   achievement_id INTEGER       NOT NULL
+,   member_id      INTEGER       NOT NULL
+,   CONSTRAINT pk_member_achievement_id PRIMARY KEY (id)
+,   CONSTRAINT fk_member_achievement_achievement_id FOREIGN KEY (achievement_id) REFERENCES achievement(id)
+,   CONSTRAINT fk_member_achievement_member_id FOREIGN KEY (member_id) REFERENCES member(id)
+,   CONSTRAINT uni_member_achievement UNIQUE (member_id, achievement_id)
 );
 
 -- 회원별 일일 미션 테이블
-CREATE TABLE IF NOT EXISTS MEMBER_DAILY_MISSION (
-    id               INTEGER       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    is_completed     VARCHAR(4)    NOT NULL DEFAULT 'N',
-    completed_date   VARCHAR(255),
-    daily_mission_id INTEGER       NOT NULL,
-    member_id        INTEGER       NOT NULL,
-    FOREIGN KEY (daily_mission_id) REFERENCES DAILY_MISSION(id) ON DELETE CASCADE,
-    FOREIGN KEY (member_id) REFERENCES MEMBER(id) ON DELETE CASCADE,
-    CONSTRAINT unique_member_daily_mission UNIQUE (member_id, daily_mission_id)
+CREATE TABLE IF NOT EXISTS MEMBER_DAILY_MISSION
+(
+    id               INTEGER       NOT NULL AUTO_INCREMENT
+,   is_completed     VARCHAR(4)    NOT NULL DEFAULT 'N'
+,   accepted_date   VARCHAR(255)
+,   daily_mission_id INTEGER       NOT NULL
+,   member_id        INTEGER       NOT NULL
+,   CONSTRAINT pk_member_daily_mission_id PRIMARY KEY (id)
+,   CONSTRAINT fk_member_daily_mission_daily_mission_id FOREIGN KEY (daily_mission_id) REFERENCES daily_mission(id)
+,   CONSTRAINT fk_member_daily_mission_member_id FOREIGN KEY (member_id) REFERENCES member(id)
+,   CONSTRAINT uni_member_daily_mission UNIQUE (member_id, daily_mission_id)
 );
 
 -- 회원별 뱃지 테이블
-CREATE TABLE IF NOT EXISTS MEMBER_BADGE (
-    id            INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    acquired_date VARCHAR(255) NOT NULL,
-    level         INTEGER      NOT NULL,
-    badge_id      INTEGER      NOT NULL,
-    member_id     INTEGER      NOT NULL,
-    FOREIGN KEY (badge_id) REFERENCES BADGE(id) ON DELETE CASCADE,
-    FOREIGN KEY (member_id) REFERENCES MEMBER(id) ON DELETE CASCADE,
-    CONSTRAINT unique_member_badge UNIQUE (member_id, badge_id)
+CREATE TABLE IF NOT EXISTS MEMBER_BADGE
+(
+    id            INTEGER      NOT NULL AUTO_INCREMENT
+,   acquired_date VARCHAR(255) NOT NULL
+,   level         INTEGER      NOT NULL
+,   badge_id      INTEGER      NOT NULL
+,   member_id     INTEGER      NOT NULL
+,   CONSTRAINT pk_member_badge_id PRIMARY KEY (id)
+,   CONSTRAINT fk_member_badge_badge_id FOREIGN KEY (badge_id) REFERENCES badge(id)
+,   CONSTRAINT fk_member_badge_member_id FOREIGN KEY (member_id) REFERENCES member(id)
+,   CONSTRAINT uni_member_badge UNIQUE (member_id, badge_id)
 );
