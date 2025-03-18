7d8f2e646d50812c4cecc2c40c3c8d4492a6d29d
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-03-19 01:19:38 +0900

    Style: 코딩 컨벤션 맞춰 수정

--- db/report/SelectReport.sql
+++ db/report/SelectReport.sql
@@ -27,19 +27,19 @@ SELECT
 
 -- 게시글(POST 테이블을 통해 회원 1번이 작성한 게시글이 신고된 경우)
 SELECT
-    R.id,
-    R.reported_at,
-    R.status,
-    R.category,
-    R.reported_id,
-    RR.reason,
-    P.id AS post_id,
-    P.member_id
-FROM REPORT R
-         JOIN REPORT_REASON RR ON R.report_reason_id = RR.id
-         JOIN POST P ON P.id = R.reported_id  -- REPORT 테이블과 POST 테이블을 조인
-WHERE R.category = 1
-  AND P.member_id = 1;
+       R.id
+     , R.reported_at
+     , R.status
+     , R.category
+     , R.reported_id
+     , RR.reason
+     , P.id AS post_id
+     , P.member_id
+  FROM REPORT R
+  JOIN REPORT_REASON RR ON R.report_reason_id = RR.id
+  JOIN POST P ON P.id = R.reported_id  -- REPORT 테이블과 POST 테이블을 조인
+ WHERE R.category = 1
+   AND P.member_id = 1;
 
 -- 댓글(COMMENT 테이블을 통해 회원 1번이 작성한 댓글이 신고된 경우)
 SELECT
@@ -60,24 +60,24 @@ WHERE R.category = 2
 -- 1번 회원 직접 신고 or 1번 회원이 작성한 게시글 신고 or 1번 회원이 작성한 댓글 신고
 -- 관리자가 이 신고를 승인한 경우 (status = 2)
 SELECT
-    R.id,
-    R.reported_at,
-    R.status,
-    R.category,
-    R.reported_id,
-    RR.reason,
-    COALESCE(P.id, C.id) AS post_or_comment_id,  -- 게시글 또는 댓글 ID
-    COALESCE(P.member_id, C.member_id) AS post_or_comment_member_id -- 게시글 또는 댓글 작성자 ID
-FROM REPORT R
-         JOIN REPORT_REASON RR ON R.report_reason_id = RR.id
-         LEFT JOIN POST P ON R.category = 1 AND P.id = R.reported_id AND P.member_id = 1
-         LEFT JOIN COMMENT C ON R.category = 2 AND C.id = R.reported_id AND C.member_id = 1
-WHERE
-    R.status = 2 AND (
-    (R.category = 0 AND R.reported_id = 1)  -- 회원이 신고된 경우
-   OR (R.category = 1 AND P.id IS NOT NULL)  -- 게시글이 신고된 경우
-   OR (R.category = 2 AND C.id IS NOT NULL)); -- 댓글이 신고된 경우
-
+       R.id
+     , R.reported_at
+     , R.status
+     , R.category
+     , R.reported_id
+     , RR.reason
+     , COALESCE(P.id, C.id) AS post_or_comment_id                      -- 게시글 또는 댓글 ID
+     , COALESCE(P.member_id, C.member_id) AS post_or_comment_member_id -- 게시글 또는 댓글 작성자 ID
+  FROM REPORT R
+  JOIN REPORT_REASON RR ON R.report_reason_id = RR.id
+  LEFT JOIN POST P ON R.category = 1 AND P.id = R.reported_id AND P.member_id = 1
+  LEFT JOIN COMMENT C ON R.category = 2 AND C.id = R.reported_id AND C.member_id = 1
+ WHERE R.status = 2
+   AND (
+           (R.category = 0 AND R.reported_id = 1)  -- 회원이 신고된 경우
+        OR (R.category = 1 AND P.id IS NOT NULL)   -- 게시글이 신고된 경우
+        OR (R.category = 2 AND C.id IS NOT NULL)   -- 댓글이 신고된 경우
+       );
 
 
 -- 규제 목록 조회
