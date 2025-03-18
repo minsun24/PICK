eb4e319f1fe294ea2b1cceaf6298d569e7f4ee37
swjang7269 <azamiyuko7269@gmail.com>
2025-03-18 14:42:29 +0900

    Refactor: 변수명 수정 및 제약조건 테이블 수정

--- db/matching/CreateMatchingTable.sql
+++ db/matching/CreateMatchingTable.sql
@@ -1,25 +1,6 @@
-DROP TABLE IF EXISTS MATCING CASCADE
 DROP TABLE IF EXISTS MATCING_ENTRY CASCADE
+DROP TABLE IF EXISTS MATCING CASCADE
 DROP TABLE IF EXISTS TECHNOLOGY_CATEGORY CASCADE
-
-CREATE TABLE IF NOT EXISTS MATCHING
-(
-  id                     INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY
-, created_date           VARCHAR(255) NOT NULL
-, is_completed           VARCHAR(4)   NOT NULL DEFAULT 'N'
-, level_range            INTEGER      NOT NULL DEFAULT 5
-, member_id              INTEGER      NOT NULL
-, technology_category_id INTEGER      NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS MATCING_ENTRY 
-(
-  id           INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY
-, applied_date VARCHAR(255) NOT NULL
-, is_cancled   VARCHAR(4)   NOT NULL DEFAULT 'N'
-, member_id    INTEGER      NOT NULL
-, matching_id  INTEGER      NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS TECHNOLOGY_CATEGORY 
 (
@@ -27,24 +8,28 @@ CREATE TABLE IF NOT EXISTS TECHNOLOGY_CATEGORY
 , name                       VARCHAR(255) NOT NULL UNIQUE
 , is_deleted                 VARCHAR(4)   NOT NULL DEFAULT 'N'
 , ref_technology_category_id INTEGER      NULL
+, CONSTRAINT fk_technology_category_id FOREIGN KEY (ref_technology_category_id) REFERENCES TECHNOLOGY_CATEGORY (id);
 );
 
-ALTER TABLE MATCHING
-  ADD CONSTRAINT fk_matching_member_id
-      FOREIGN KEY (member_id) REFERENCES MEMBER(id);
+CREATE TABLE IF NOT EXISTS MATCHING
+(
+  id                     INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY
+, created_date_at        VARCHAR(255) NOT NULL
+, is_completed           VARCHAR(4)   NOT NULL DEFAULT 'N'
+, level_range            INTEGER      NOT NULL DEFAULT 5
+, member_id              INTEGER      NOT NULL
+, technology_category_id INTEGER      NOT NULL
+, CONSTRAINT fk_matching_member_id FOREIGN KEY (member_id) REFERENCES MEMBER (id)
+, CONSTRAINT fk_matching_technology_category_id FOREIGN KEY (technology_category_id) REFERENCES TECHNOLOGY_CATEGORY (id)
+);
 
-ALTER TABLE MATCHING
-  ADD CONSTRAINT fk_matching_technology_category_id
-      FOREIGN KEY (technology_category_id) REFERENCES TECHNOLOGY_CATEGORY(id);
-      
-ALTER TABLE MATCHING_ENTRY
-  ADD CONSTRAINT fk_matching_entry_member_id
-      FOREIGN KEY (member_id) REFERENCES MEMBER(id);
-
-ALTER TABLE MATCHING_ENTRY
-  ADD CONSTRAINT fk_matching_entry_matching_id
-      FOREIGN KEY (matching_id) REFERENCES MATCHING(id);
-
-ALTER TABLE TECHNOLOGY_CATEGORY
-  ADD CONSTRAINT fk_technology_category_id
-      FOREIGN KEY (ref_technology_category_id) REFERENCES TECHNOLOGY_CATEGORY(id);
+CREATE TABLE IF NOT EXISTS MATCING_ENTRY 
+(
+  id           INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY
+, applied_date_at VARCHAR(255) NOT NULL
+, is_cancled   VARCHAR(4)   NOT NULL DEFAULT 'N'
+, member_id    INTEGER      NOT NULL
+, matching_id  INTEGER      NOT NULL
+, CONSTRAINT fk_matching_entry_member_id FOREIGN KEY (member_id) REFERENCES MEMBER(id)
+, CONSTRAINT fk_matching_entry_matching_id FOREIGN KEY (matching_id) REFERENCES MATCHING(id);
+);
