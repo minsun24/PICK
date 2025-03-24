b8e30b0ec8434306b21bf937f668ee30391346dc
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-03-24 12:25:47 +0900

    Feat: Eureka 서버 및 Gateway, 로드밸런싱 적용

--- pick-gateway/src/main/java/com/nob/pick/pickgateway/PickGatewayApplication.java
+++ pick-gateway/src/main/java/com/nob/pick/pickgateway/PickGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.nob.pick;
+package com.nob.pick.pickgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PickApplication {
+public class PickGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PickApplication.class, args);
+        SpringApplication.run(PickGatewayApplication.class, args);
     }
 
 }
