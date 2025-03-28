2e7e8e9150ac2645219c106a5b09e19500e5bd75
Bluesky3125 <charlie4822@gmail.com>
2025-03-28 17:29:18 +0900

    Refactor: 게시글 삭제 메소드에 현재 회원 검증 추가 외 6건
    Feat: Member 서비스 클라이언트에 회원 id로 회원 정보 받아오는 메소드 추가
    Refactor: 게시글 등록 메소드에 현재 회원을 토큰으로 가져오게 변경
    Refactor: 게시글 삭제 메소드에 현재 회원 검증 추가
    Comment: 서비스의 deletePost 메소드 주석 추가
    Feat: Pick의 JwtUtil에 getId 추가
    Refactor: JwtUtil에 토큰 생성 기능 주석 처리 및 그에 따른 미사용되는 의존성 주입 주석 처리
    Refactor: WebSecurity 패키지 변경... 및 그에 따른 불필요한 import 제거

--- Pick/src/main/java/com/nob/pick/infrastructure/MemberServiceClient.java
+++ Pick/src/main/java/com/nob/pick/infrastructure/MemberServiceClient.java
@@ -1,7 +1,11 @@
 package com.nob.pick.infrastructure;
 
+
 import com.nob.pick.matching.query.vo.ResponseMemberProfileVO;
+import com.nob.pick.member.query.dto.MemberDTO;
+
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,4 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface MemberServiceClient {
     @GetMapping("/pick-member-service/api/members/profile-page/{memberId}")
     ResponseMemberProfileVO getMemberProfileByMemberId(@PathVariable("memberId") int memberId);
+    
+    @GetMapping("/pick-member-service/api/members/{id}")
+    ResponseEntity<MemberDTO> getMemberById(@PathVariable("id") int memberId);
 }
